8d8f7aa03d83024c7796bbfae3ed0bd46af6852d
xa <OldAugustus@outlook.com>
2022-12-31 13:04:25 +0800

    buildFuture

--- git_deploy.bat
+++ git_deploy.bat
@@ -4,11 +4,11 @@ git status
 set /p msg=Comment:
 git add .
 git commit -m %msg%
-git push -u origin hugo-main
+git push -u origin hugo-main --force
 cd public
 git add .
 git commit -m %msg%
-git push -u origin gh-pages
+git push -u origin gh-pages --force
 cd ..
 echo Push successfully ![%msg%]
 echo "--------END--------"
